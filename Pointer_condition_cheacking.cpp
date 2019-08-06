fc1535014cbe453f59a320eff79b7bc85f04881f
SaimunJd <50344773+SaimunJd@users.noreply.github.com>
2019-08-06 12:44:31 +0600

    Create Pointer_condition_cheacking.cpp

---
+++ Pointer_condition_cheacking.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char ch[100];
+    int a;
+    int *p = NULL;
+    if(!p){
+        cout<<"saimun";//its true because pointer p is null
+        }
+      if(p){
+          cout<<"srabon";//its false
+          }
+	getch();
+}
